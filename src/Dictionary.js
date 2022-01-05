ceaa80b45be4177cd776e4e8cee92092b2c131ed
{elimdd} <{elimdd@gmail.com}>
2022-01-05 07:18:31 -0300

    Axios API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,25 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import axios from 'axios'
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState ("");
 
+function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
     function searching (event) {
         event.preventDefault();
         alert(`Searching for ${keyword} definition...`);
-    }
+    
+        //documentation: https://dictionaryapi.dev/
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log();
+    axios.get(apiUrl).then(handleResponse);
+  }
 
 function handleTyping (event) {
 console.log(event.target.value);
