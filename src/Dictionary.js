c2c957a8f45ed3739f8ac8d9e3cdf526d9a65c93
{elimdd} <{elimdd@gmail.com}>
2022-01-05 07:00:17 -0300

    Form setup

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 import Results from "./Results";
 
 export default function Dictionary() {
+let [keyword, setKeyword] = useState ("");
+
+    function searching (event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition...`);
+    }
+
+function handleTyping (event) {
+console.log(event.target.value);
+setKeyword(event.target.value);
+}
+
   return (
     <div className="dictionary">
-    <form>
-      <input type="search"></input>
+    <form onSubmit={searching}>
+      <input type="search" onChange={handleTyping} ></input>
     </form>
     <Results />
   </div>
