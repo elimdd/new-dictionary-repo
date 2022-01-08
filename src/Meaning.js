f90380b9eec46a2f1944dd7c8de1d3aa03be3b70
{elimdd} <85963168+elimdd@users.noreply.github.com>
2022-01-08 11:38:13 -0300

    Meaning results update

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,10 +3,17 @@ import React from "react";
 export default function Meaning(props) {
     console.log(props.meaning);
     return (
-        <div>
+        <div className="meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definitions[0].definition}</p>
-        <p>{props.meaning.definitions[0].exemple}</p>
+        {props.meaning.definitions.map(function(definition, index) {
+            return (
+                <div key={index}>
+                    {definition.definition}
+                    <br />
+                    <em>{definition.example}</em>
+                    </div>
+            )
+        })}
         </div>
     );
 }
